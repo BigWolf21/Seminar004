d03ef57f0211563d31ceb45508f6d50745d9693a
BigWolf21 <122544707+BigWolf21@users.noreply.github.com>
2023-02-25 20:45:07 +0700

    Создание HW01 метод решения 1-ой задачи

---
+++ HW01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите метод, который принимает 
+// на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 
+// 2, 4 -> 16
+
+int GetNumber(string text)
+{
+  Console.WriteLine(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int Main(int numberA, int numberB)
+{
+int degree = numberA;
+int index = 0;
+for (index=1; index < numberB; index++)
+{
+  degree = degree * numberA;
+}
+return degree;
+}
+
+int numberA = GetNumber("Введите число A ");
+int numberB = GetNumber("Введите число B ");
+int degree = Main(numberA, numberB);
+Console.WriteLine("Число " + numberA + " в степени " + numberB + " равно " + degree);
