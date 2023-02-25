5e47c93b9a65b6bf042e4efe3d0fee77b740b7b3
BigWolf21 <122544707+BigWolf21@users.noreply.github.com>
2023-02-25 22:50:05 +0700

    Создание HW03 решения 3-ей задачи, через методы

---
+++ HW03/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 29: Напишите метод, который задаёт массив
+// из N элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int GetNumber(string text)
+{
+  Console.WriteLine(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+  return new int[size];
+}
+
+void FillByUser(int[] array)
+{
+  int size = array.Length;
+  int index = 0;
+  while (index < size)
+  {
+    array[index] = GetNumber("Введите " + index + "элемент: ");
+    index++;
+  }
+
+}
+
+string Print(int[] array)
+{
+  string result = String.Empty;
+  int len = array.Length;
+  int index = 0;
+  while (index < len)
+  {
+    result = result + " " + array[index];
+    index++;
+  }
+  return result;
+}
+
+int size = GetNumber("Введите размер массива ");
+int[] value = CreateArray(size);
+FillByUser(value);
+Console.WriteLine(Print(value));
